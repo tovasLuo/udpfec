2a8bccd4ecd266ba1313fdfadac9df7d26bb1c12
luojing <luojing>
2026-06-18 11:28:35 +0800

    add win调用源代码

---
+++ src/win/FecAlgorithm.h
@@ -0,0 +1,75 @@
+#pragma once
+
+//pkt
+using fec_up_callback = std::function<void(sUdpPacketPtr pkt)>;
+
+//ec,pkt,close
+using fec_channel_up = asio::experimental::concurrent_channel<
+	void(error_code, sUdpPacketPtr, sUdpConstBufferPtr, const sockaddr_full, sUdpSockInfoPtrC, bool exit)>;
+using fec_channel_down = asio::experimental::concurrent_channel<void(error_code, sUdpPacketPtr, bool exit)>;
+
+class FecAlgorithm
+	: private fec::callback
+
+{
+public:
+	FecAlgorithm(u32 udppwd, fec_up_callback send_cb, udp_pkt_callback recv_cb)
+		: _channel_up(_ctx, 10000)
+		, _channel_down(_ctx, 10000)
+		, _send_cb(send_cb)
+		, _recv_cb(recv_cb)
+		, _cfg_udppwd(udppwd)
+	{
+		TRACEX0("FecAlgorithm new  begin");
+		_start();
+		TRACEX0("FecAlgorithm new  end");
+	}
+	~FecAlgorithm()
+	{
+		TRACEX0("FecAlgorithm free begin");
+		_stop();
+		TRACEX0("FecAlgorithm free end");
+	}
+
+	// ͨ�� IUdpNetworkProcessor �̳�
+	void WriteClose(sUdpSockInfoPtrC info);
+	void WriteSend(sUdpPacketPtr pkt, sUdpConstBufferPtr buf_head, const sockaddr_full& ep_transer);
+	void WriteRecv(sUdpPacketPtr pkt);
+	void SignalExit();
+
+private:
+	virtual void send_fec_pdu_cb(fec::StreamKey streamKey, const sockaddr_full& epNode, const u8* data, u16 size, void* context) override;
+	virtual void recv_pdu_cb(fec::StreamKey streamKey, const sockaddr_full& epNode, const sockaddr_full& epTranserLocal, const u8* data, u16 size, void* context) override;
+
+private:
+	void _start();
+	void _stop();
+
+	auto _coro_main() -> awaitable<void>;
+	auto _coro_main_down(fec& fec) -> awaitable<void>;
+	auto _coro_main_fec_poll(fec& fec, bool& fec_poll_running) -> awaitable<void>;
+
+private:
+
+	/* config */
+	const u32 _cfg_udppwd;
+
+	fec_up_callback		_send_cb;
+	udp_pkt_callback	_recv_cb;
+
+	/* status */
+	std::map<fec::StreamKey, sUdpSockInfoPtrC>	_info;
+	std::map<u64, SmallSet<fec::StreamKey, 20>>	_streamKeys;	//key=nid
+
+
+	/* �ⲿ�߳� */
+
+	/* FEC�߳� */
+	io_context			_ctx;
+	std::thread			_th;
+	fec_channel_up		_channel_up;
+	fec_channel_down	_channel_down;
+
+};
+
+using FecAlgorithmPtr = std::shared_ptr<FecAlgorithm>;
---
+++ src/win/fec.h
@@ -0,0 +1,75 @@
+#pragma once
+
+class fec_env
+{
+public:
+    fec_env();
+    ~fec_env();
+
+    operator bool() const;
+    std::string get_ver() const;
+
+private:
+    bool env_ok_;
+};
+
+class fec
+{
+public:
+    using StreamKey = u64;
+
+    class callback
+    {
+    public:
+		virtual void send_fec_pdu_cb(StreamKey streamKey, const sockaddr_full& epNode, const u8* data, u16 size, void* context) = 0;
+        virtual void recv_pdu_cb(StreamKey streamKey, const sockaddr_full& epNode, const sockaddr_full& epTranserLocal, const u8* data, u16 size, void* context) = 0;
+        virtual void on_session_closed_cb(StreamKey streamKey) {}
+    };
+
+    struct ref_buf
+    {
+        const u8* data;
+        u16 len;
+
+        ref_buf(const u8* _data, u16 _len) {
+            data = _data;
+            len = _len;
+        }
+    };
+
+public:
+    fec(callback* cb);
+    ~fec();
+
+    operator bool() const;
+
+    bool fec_wheel();
+    static bool is_fec_pkg(uint8_t* data, size_t size, StreamKey* streamKey = nullptr);
+
+    bool recv_fec_pdu(StreamKey streamKey, const sockaddr_full& epLocal, const sockaddr_full& epRemote, const u8* data, u16 len);
+    static uint32_t recv_pdu_cb(GtpHandler_p gtp_hdl, void* data, uint32_t size, GtpAddr* client_gtp_addr);
+
+    bool send_pdu(StreamKey streamKey, const sockaddr_full& epLocal, const sockaddr_full& epRemote, const u8* data, u16 len);
+	bool send_pdu(StreamKey streamKey, const sockaddr_full& epLocal, const sockaddr_full& epRemote, const std::vector<fec::ref_buf>& bufs);
+
+    static uint32_t send_fec_pdu_cb(GtpHandler_p gtp_hdl, void* data, uint32_t size, GtpAddr* client_gtp_addr);
+
+    static void fec_log_cb(uint32_t log_level, const char* fmt, ...);
+    static uint32_t fec_log_level_cb();
+    static void close_session_cb(GtpHandler_p gtp_hdl, void* context);
+
+    static uint32_t report_link_quality_cb(GtpHandler_p gtp_hdl, GtpLinkQuality vec[], uint32_t size);
+    void del_fec_session(StreamKey streamKey, const sockaddr_full& epLocal, const sockaddr_full& epRemote);
+    void del_fec_session(StreamKey streamKey);
+
+private:
+    GtpHandler_p hdl_;
+    u32 thread_id_;
+
+    static std::shared_mutex map_cb_mtx_;
+    static std::map<u32, callback*> map_cb_;
+
+};
+
+_declspec(selectany) std::shared_mutex fec::map_cb_mtx_;
+_declspec(selectany) std::map<u32, fec::callback*> fec::map_cb_;
